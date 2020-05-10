7245eaffb47a1b96ffaffd1f09a4882453ad251b
Maurice73-Tech <63903249+Maurice73-Tech@users.noreply.github.com>
2020-05-10 11:44:33 +0200

    Update CarDashboard.R

--- Dashboard/CarDashboard.R
+++ Dashboard/CarDashboard.R
@@ -1,30 +1,66 @@
 #Simple R-Dasboard 
 library(shinydashboard)
 
+#ui 
 ui <- dashboardPage(
-  dashboardHeader(title = "Car Dashboard"),
-  dashboardSidebar(),
+  #header 
+  dashboardHeader(title = "Car Dashboard",
+                  dropdownMenu(type = "messages",
+                               messageItem(
+                                 from = "Sales Dept",
+                                 message = "Sales are steady this month."
+                               ),
+                               messageItem(
+                                 from = "New User",
+                                 message = "How do I register?",
+                                 icon = icon("question"),
+                                 time = Sys.time()
+                               ),
+                               messageItem(
+                                 from = "Support",
+                                 message = "The new server is ready.",
+                                 icon = icon("life-ring"),
+                                 time = Sys.Date()
+                               )
+                  )),
+  #sidebar
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "Dashboard", icon = icon("Dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  #body
   dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 250)),
-
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50)
+    tabItems(
+      tabItem(tabName = "Dashboard", 
+              fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+            ),
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content")
+              )
       )
     )
-  )
+    
 )
 
+#server
 server <- function(input, output) {
   set.seed(122)
   histdata <- rnorm(500)
-
+  
   output$plot1 <- renderPlot({
     data <- histdata[seq_len(input$slider)]
     hist(data)
   })
 }
 
+#Perform the App
 shinyApp(ui, server)
