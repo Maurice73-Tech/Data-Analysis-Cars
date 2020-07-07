b54d86fadafaa1b55dbfcaf712e82cbdbcc3c373
Maurice73-Tech <63903249+Maurice73-Tech@users.noreply.github.com>
2020-07-07 17:29:19 +0200

    Update Cars.R

--- Car-Analysis/Cars.R
+++ Car-Analysis/Cars.R
@@ -1,6 +1,13 @@
 #R-Data Analysis with the mtcars Dataset 
+
+#Install Packages
+install.packages('plotly')
+
+#Import libraries
 library(ggplot2)
 library(data.table)
+library(ggthemes)
+library(plotly)
 
 #Print the mtcars dataset
 cars <- mtcars
@@ -20,3 +27,29 @@ print(a2)
 bp <- ggplot(cars, aes(factor(cyl), hp))
 bp2 <- bp + geom_boxplot(aes(fill=factor(cyl)))
 print(bp2)
+
+#Scatterplot
+head(cars)
+sp <- ggplot(cars, aes(x=hp, y = qsec))
+sp2 <- sp + geom_point(aes(color=factor(qsec)))
+print(sp2)
+
+#Different themes
+th1 <- ggplot(cars, aes(x=mpg,y=hp)) + geom_point()
+print(th1)
+th2 <-th1 + theme_bw()
+th3 <-th1 + theme_classic()
+th4 <-th1 + theme_dark()
+th5 <-th1 + theme_economist_white()
+th6 <-th1 + theme_wsj()
+print(th6) 
+
+#Interactive diagram with plotly
+abc <- ggplot(cars, aes(mpg, wt)) + geom_point()
+abc2 <- abc + theme_wsj()
+plotly <- ggplotly(abc2)
+print(plotly)
+
+#Prediction
+my_pr <- lm(mpg ~ cyl + hp, data = cars)
+predict(my_pr, data.frame(cyl = 12, hp = 400))
